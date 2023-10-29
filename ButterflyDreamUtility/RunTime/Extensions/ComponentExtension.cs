b2307da80796a9b81be6e123b94f98c515f17601
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-10-29 14:27:13 +0900

    Add: 空処理のグラフィックとコンポーネント検索拡張メソッドの実装
    (cherry picked from commit c1abcddef75b8ddb6b0bbf24a903c8acc5547ea0)

---
+++ ButterflyDreamUtility/RunTime/Extensions/ComponentExtension.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ButterflyDreamUtility.Extensions
+{
+    public static class ComponentExtension
+    {
+        /// <summary>
+        /// 子オブジェクト以下のComponentを取得する
+        /// </summary>
+        /// <param name="target">対象のゲームオブジェクト</param>
+        /// <param name="includeInactive">非アクティブのオブジェクトのコンポーネントも含めるかどうか</param>
+        /// <param name="includeRoot">対象のルートゲームオブジェクトのコンポーネントも含めるかどうか</param>
+        /// <typeparam name="T">取得するコンポーネントの型</typeparam>
+        /// <returns>子オブジェクト以下の任意のComponent群</returns>
+        public static T[] GetComponentsInChildren<T>(this Component target, bool includeInactive, bool includeRoot) where T : Component
+        {
+            if (includeRoot) return target.GetComponentsInChildren<T>(includeInactive);
+            List<T> components = new List<T>();
+            target.GetComponentsInChildren(includeInactive, components);
+            foreach (T targetComponent in target.GetComponents<T>())
+            {
+                if (components.Contains(targetComponent))
+                {
+                    components.Remove(targetComponent);
+                }
+            }
+            return components.ToArray();
+        }
+
+        /// <summary>
+        /// 子オブジェクトのみのComponentを取得する（親も孫も含めない）
+        /// </summary>
+        /// <param name="target">対象のゲームオブジェクト</param>
+        /// <param name="includeInactive">非アクティブのオブジェクトのコンポーネントも含めるかどうか</param>
+        /// <typeparam name="T">取得するコンポーネントの型</typeparam>
+        /// <returns>子オブジェクトのみの任意のComponent群</returns>
+        public static T[] GetComponentsInOnlyChildren<T>(this Component target, bool includeInactive = false) where T : Component
+        {
+            List<T> components = new List<T>();
+            Transform parent = target.transform;
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                Transform child = parent.GetChild(i);
+                if(includeInactive && !child.gameObject.activeSelf) continue;
+                if(child.TryGetComponent(out T component))
+                {
+                    components.Add(component);
+                }
+            }
+            return components.ToArray();
+        }
+    }
+}
---
+++ ButterflyDreamUtility/RunTime/UI/EmptyGraphic.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ButterflyDreamUtility.UI
+{
+    using Extensions;
+
+    /// <summary>
+    /// 空処理のグラフィック
+    /// <remarks>
+    /// 想定用途：マスク時のベース, 画面全体を透明なuGUIで覆いたいとき、Buttonの判定領域を透明にしたいとき
+    /// </remarks>
+    /// </summary>
+    [RequireComponent(typeof(CanvasRenderer))]
+    public sealed class EmptyGraphic : Graphic
+    {
+        /// <summary>
+        /// 子オブジェクト以下のGraphic（自分は含まない）
+        /// </summary>
+        private Graphic[] childGraphics = null;
+
+        /// <inheritdoc/>
+        public override Color color
+        {
+            get => base.color;
+            set
+            {
+                // 子オブジェクトの色は変更する
+                foreach (var childGraphic in childGraphics)
+                {
+                    childGraphic.color = value;
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void Start()
+        {
+            base.Start();
+            childGraphics = this.GetComponentsInChildren<Graphic>(true, false);
+        }
+
+        /// <inheritdoc/>
+        public override void SetMaterialDirty()
+        {
+            // 空処理を上書き
+        }
+
+        /// <inheritdoc/>
+        public override void SetVerticesDirty()
+        {
+            // 空処理を上書き
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}
