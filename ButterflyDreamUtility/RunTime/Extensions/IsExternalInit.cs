c32013a62a6a4615dda261dea6bb970b741df569
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-12-17 23:53:10 +0900

    Fix: init-onlyアクセサ周りの整備
    (cherry picked from commit 696610300f80ad780e223a3a4a4cfbaac3422182)

---
+++ ButterflyDreamUtility/RunTime/Extensions/IsExternalInit.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices
+{
+#if !NET5_0_OR_GREATER
+    // init-onlyアクセサ & record型を使用するための拡張
+    public sealed class IsExternalInit { }
+#endif
+}
--- ButterflyDreamUtility/RunTime/Extensions/RecordTypeExtension.cs
+++
@@ -1,5 +0,0 @@
-﻿// Record型を使用するための拡張
-namespace System.Runtime.CompilerServices
-{
-    public class IsExternalInit { }
-}
