11bd9fa0e6b6e5651c786c64392a013b56dde853
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-12-18 00:48:22 +0900

    Fix: SimpleScrollViewの調整とVector系拡張メソッドの充足
    (cherry picked from commit 970d9119e125cfc8564b6133bc42a9aa922cc375)

--- ButterflyDreamUtility/RunTime/Extensions/VectorExtension.cs
+++ ButterflyDreamUtility/RunTime/Extensions/VectorExtension.cs
@@ -199,6 +199,78 @@ namespace ButterflyDreamUtility.Extensions
         /// <param name="min">最小値</param>
         /// <param name="max">最大値</param>
         /// <returns></returns>
+        public static Vector3 Clamp(ref Vector3 value, ref Vector3 min, Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector3 Clamp(ref Vector3 value, Vector3 min, ref Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector3 Clamp(Vector3 value, ref Vector3 min, ref Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector3 Clamp(ref Vector3 value, Vector3 min, Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector3 Clamp(Vector3 value, ref Vector3 min, Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector3 Clamp(Vector3 value, Vector3 min, ref Vector3 max)
+        {
+            return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
         public static Vector3 Clamp(Vector3 value, Vector3 min, Vector3 max)
         {
             return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
@@ -212,6 +284,78 @@ namespace ButterflyDreamUtility.Extensions
         /// <param name="max">最大値</param>
         /// <returns></returns>
         public static Vector2 Clamp(ref Vector2 value, ref Vector2 min, ref Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(ref Vector2 value, ref Vector2 min, Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(ref Vector2 value, Vector2 min, ref Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(Vector2 value, ref Vector2 min, ref Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(ref Vector2 value, Vector2 min, Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(Vector2 value, ref Vector2 min, Vector2 max)
+        {
+            return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+        }
+
+        /// <summary>
+        /// 指定した範囲内に収めた値のコピーを返す
+        /// </summary>
+        /// <param name="value">対象の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns></returns>
+        public static Vector2 Clamp(Vector2 value, Vector2 min, ref Vector2 max)
         {
             return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
         }
