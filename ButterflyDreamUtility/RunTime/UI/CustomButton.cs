f14dcd9ba9d93ae0ababd7d16f59df5d06d75572
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-01-15 17:09:54 +0900

    Fix: ボタンの改良
    (cherry picked from commit 9dc11c9c7009352bcef215ba4198adc12821df2f)

--- ButterflyDreamUtility/RunTime/UI/CustomButton.cs
+++ ButterflyDreamUtility/RunTime/UI/CustomButton.cs
@@ -15,47 +15,75 @@ namespace ButterflyDreamUtility.UI
     public sealed class CustomButton : Selectable, IPointerClickHandler
     {
         /// <summary>
+        /// ボタン押下時のクリック間隔（ミリ秒）
+        /// </summary>
+        private const int CLICK_INTERVAL_MS = 500;
+
+        /// <summary>
+        /// クリックされたかどうか（連打対策）
+        /// </summary>
+        private bool isClicked = false;
+
+        /// <summary>
+        /// destroyCancellationTokenのキャッシュ
+        /// </summary>
+        private CancellationToken cancellationToken;
+
+        /// <summary>
         /// ボタン押下時に発火するイベント
         /// </summary>
         public event Action onClick = null;
 
         /// <summary>
-        /// ボタン押下時に発火するイベント（非同期で待機させることで排他制御する）
-        /// <remarks>
-        /// 引数にキャンセルトークンを渡して呼び出す
-        /// </remarks>
+        /// 有効なCustomButtonを全て取得する
         /// </summary>
-        public event Func<CancellationToken, UniTask> onClickLockAsync = null;
-
-        /// <summary>
-        /// クリックされたかどうか（連打対策のためにクリックされてからしばらくtrueのまま）
-        /// </summary>
-        private bool isClicked = false;
-
-        /// <summary>
-        /// 同一ボタンを二本以上の指で押している際の該当接触数（そんなに押さないで...）
-        /// </summary>
-        private uint touchCount = default;
-
-        /// <summary>
-        /// 何かしら状態が変わっているボタン群
-        /// </summary>
-        private static readonly List<CustomButton> activeStateButtons = new List<CustomButton>(10);
-
-        /// <inheritdoc/>
-        public override void OnPointerEnter(PointerEventData eventData)
+        public IEnumerable<CustomButton> allCustomButtons
         {
-            base.OnPointerEnter(eventData);
-            touchCount++;
-            activeStateButtons.Add(this);
+            get
+            {
+                List<CustomButton> result = new List<CustomButton>(s_SelectableCount);
+                for (int i = 0; i < s_SelectableCount; i++)
+                {
+                    if (s_Selectables[i] is CustomButton
+                        {
+                            currentSelectionState: not SelectionState.Disabled
+                        })
+                    {
+                        result.Add((CustomButton) s_Selectables[i]);
+                    }
+                }
+                result.TrimExcess();
+                return result;
+            }
         }
 
-        /// <inheritdoc/>
-        public override void OnPointerExit(PointerEventData eventData)
+        /// <summary>
+        /// 既に押されているCustomButtonを全て取得する
+        /// </summary>
+        private IReadOnlyCollection<CustomButton> allOnClickCustomButtons
         {
-            base.OnPointerExit(eventData);
-            touchCount--;
-            activeStateButtons.Remove(this);
+            get
+            {
+                List<CustomButton> result = new List<CustomButton>(s_SelectableCount);
+                for (int i = 0; i < s_SelectableCount; i++)
+                {
+                    if (s_Selectables[i] is CustomButton
+                        {
+                            currentSelectionState: not SelectionState.Disabled and not SelectionState.Normal
+                        })
+                    {
+                        result.Add((CustomButton) s_Selectables[i]);
+                    }
+                }
+                result.TrimExcess();
+                return result;
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            cancellationToken = destroyCancellationToken;
         }
 
         /// <inheritdoc/>
@@ -67,38 +95,44 @@ namespace ButterflyDreamUtility.UI
             // ボタンが無効なら無視
             if (!IsActive() || !IsInteractable()) return;
 
-            // アクティブなボタンに登録がないなら無視（同時押し対策）& 接触数が二点以上なら無視（同一ボタン重複押し対策）
-            if(!activeStateButtons.Contains(this) || touchCount > 1) return;
-
-            // 同時に押していた他のボタン群の状態をクリア
-            foreach (CustomButton activeStateButton in activeStateButtons)
-            {
-                if (activeStateButton == this) continue;
-                // 演出状態のリセット
-                activeStateButton.InstantClearState();
-            }
-            activeStateButtons.RemoveAll(btn => btn != this);
-
-            // 連打対策
+            // 連打・同時押し対策
             if (isClicked) return;
-            isClicked = true;
-            UniTask.Void(async token =>
-            {
-                // 厳密に排他制御する場合
-                if (onClickLockAsync != null)
-                {
-                    // 登録されていた非同期イベントの発火・待機
-                    await onClickLockAsync(token);
-                }
-                else  // 適当に連打対策したい場合
-                {
-                    await UniTask.Delay(TimeSpan.FromSeconds(colors.fadeDuration), cancellationToken: token);
-                }
-                isClicked = false;
-            }, destroyCancellationToken);
 
             // 登録されていた同期イベントの発火
             onClick?.Invoke();
+
+            // 同時押し対策
+            var customButtons = allOnClickCustomButtons;
+            List<UniTask> tasks = new List<UniTask>(customButtons.Count - 1);
+            foreach (CustomButton btn in customButtons)
+            {
+                tasks.Add(UniTask.Defer(async () =>
+                {
+                    await UniTask.WaitUntil(() => btn.currentSelectionState == SelectionState.Normal, cancellationToken: cancellationToken);
+                }));
+            }
+
+            UniTask.Void(async uniTasks =>
+            {
+                foreach (CustomButton customButton in allCustomButtons)
+                {
+                    customButton.isClicked = true;
+                }
+                // すべてのボタンの状態がNormalになるまで待機
+                await UniTask.WhenAll(uniTasks);
+                foreach (CustomButton customButton in allCustomButtons)
+                {
+                    if(customButton == this) continue;
+                    customButton.isClicked = false;
+                }
+            }, tasks);
+
+            // 連打対策
+            UniTask.Void(async () =>
+            {
+                await UniTask.Delay(CLICK_INTERVAL_MS, cancellationToken: cancellationToken);
+                isClicked = false;
+            });
         }
     }
 }
